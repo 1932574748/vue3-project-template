99df95fd1badf5d95f16014f208ccc15d2b66bff
ximuqi <mr_ximu@163.com>
2023-09-24 14:59:46 +0800

    feat：安装Pinia的使用1：未持久化

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,13 @@ import App from './App.vue'
 //1、route
 import router from '@/router/index'
 
+//2、pinia
+import pinia from '@/store'
+
 const app = createApp(App)
 
 //1、route
 app.use(router)
+//2、pinia
+app.use(pinia)
 app.mount('#app')
