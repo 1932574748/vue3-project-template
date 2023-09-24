6018f60c7141f3fdb3148dc5cc7d9bd2e9f5c877
ximuqi <mr_ximu@163.com>
2023-09-24 10:14:37 +0800

    feat：配置src别名为@、src/components别名为@_c

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//1、 导入 path 模块，帮助我们解析路径
+import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+    ],
+
+    //1、 ↓解析配置
+    resolve: {
+        // ↓路径别名
+        alias: {
+            "@": resolve(__dirname, "./src"),
+            "_c": resolve(__dirname, "./src/components")
+        }
+    }
 })
