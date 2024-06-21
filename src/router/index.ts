297d88ad4d51fcf109c6846f2c6555af83d1c8b4
YangDH <83196066+1932574748@users.noreply.github.com>
2024-06-21 16:38:50 +0800

    不同设备路由分发

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,57 @@
 import { createRouter, createWebHistory } from "vue-router"
-
-const routes = [
+//PC端
+const routerPc = [
     {
         path: '/',
         redirect: 'index',
     },
     {
         path: '/index',
-        name: 'Home',
+        name: 'Home',//要保持唯一性
         component: () => import('@/views/index.vue'),
     },
     {
         path: '/about',
         name: 'About',
-        //使用import可以路由懒加载，如果不使用，太多组件一起加载会造成白屏
         component: () => import('@/views/about/index.vue')
     },
     {
         path: '/dialog',
         name: 'Dialog',
-        //使用import可以路由懒加载，如果不使用，太多组件一起加载会造成白屏
         component: () => import('@/views/dialog/index.vue')
     },
     {
         path: '/dashboard',
         name: 'Dashboard',
-        //使用import可以路由懒加载，如果不使用，太多组件一起加载会造成白屏
         component: () => import('@/views/dashboard/index.vue')
     },
+]
+//移动端
+const routesMove = [
+    {
+        path: '/m/index',
+        name: 'MobileHome',//要保持唯一性
+        component: () => import('@/views/index.vue'),
+    },
+    {
+        path: '/m/about',
+        name: 'MobileAbout',
+        component: () => import('@/views/about/index.vue')
+    },
+    {
+        path: '/m/dialog',
+        name: 'MobileDialog',
+        component: () => import('@/views/dialog/index.vue')
+    },
+    {
+        path: '/m/dashboard',
+        name: 'MobileDashboard',
+        component: () => import('@/views/dashboard/index.vue')
+    },
+
+]
+//其他路由
+const routesOther = [
     {
         // 配置404页面
         path: '/:catchAll(.*)',
@@ -35,10 +59,31 @@ const routes = [
         component: () => import('@/views/404/index.vue'),
     }
 ]
+const routes = [...routerPc, ...routesMove, ...routesOther]
 // 路由
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+//路由分发
+router.beforeEach((to, from, next) => {
+    // 是否是移动端设备
+    const isMobile =
+        /(Android|webOS|iPhone|iPod|tablet|BlackBerry|Mobile)/i.test(
+            navigator.userAgent
+        );
+
+    // 是否是手机端路由
+    const isRouterMobile = /^\/m\//.test(to.fullPath);
+    // 移动端并且 不是/m开头路由
+    if (isMobile && !isRouterMobile) {
+        next('/m' + to.fullPath);
+    } else if (!isMobile && isRouterMobile) {
+        next(to.fullPath.replace('/m', ''));
+    } else {
+        next();
+    }
+});
+
 // 导出
 export default router
